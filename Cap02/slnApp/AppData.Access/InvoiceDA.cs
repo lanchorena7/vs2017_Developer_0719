5702c3809e7b9fb84749bc0307a4c492c2470cd6
lanchorena7 <52942950+lanchorena7@users.noreply.github.com>
2019-07-24 22:24:58 -0500

    .
    .

--- Cap02/slnApp/AppData.Access/InvoiceDA.cs
+++ Cap02/slnApp/AppData.Access/InvoiceDA.cs
@@ -1,12 +1,92 @@
-﻿using System;
+﻿using AppEntities.Base;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AppData.Access
 {
-    class InvoiceDA
+    public class InvoiceDA :BaseDA
     {
+        public int InsertInvoice(Invoice invoice)
+        {
+            int result = 0;
+            using (IDbConnection cnx = new SqlConnection(ConnectionString))
+            {
+                cnx.Open();
+                //Inicilizando la transacción
+                var transaction = cnx.BeginTransaction(); // Aqui se da el inicio a la transacción
+                try
+                {
+                    var commandCab = cnx.CreateCommand();
+                    commandCab.CommandText = "usp_InsertInvoce";
+                    commandCab.CommandType = CommandType.StoredProcedure;
+
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@CustomerId", invoice.CustomerId)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@InvoiceDate", invoice.InvoiceDate)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@BillingAddress", invoice.BillingAddress)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@BillingCity", invoice.BillingCity)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@BillingState", invoice.BillingState)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@BillingCountry", invoice.BillingCountry)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@BillingPostalCode", invoice.BillingPostalCode)
+                        );
+                    commandCab.Parameters.Add(
+                        new SqlParameter("@Total", invoice.Total)
+                        );
+
+                    //Ejecutando y obteniendo el codigo autogenerado
+                     var id = Convert.ToInt32(commandCab.ExecuteScalar());
+
+                    foreach (var line in invoice.InvoiceLine)
+                    {
+                        var commandDet = cnx.CreateCommand();
+                        commandDet.CommandText = "usp_InsertInvoceLine";
+                        commandDet.CommandType = CommandType.StoredProcedure;
+
+                        commandCab.Parameters.Add(
+                            new SqlParameter("@InvoiceId", id)
+                            );
+                        commandCab.Parameters.Add(
+                            new SqlParameter("@TrackId", line.TrackId)
+                            );
+                        commandCab.Parameters.Add(
+                            new SqlParameter("@UnitPrice", line.UnitPrice)
+                            );
+                        commandCab.Parameters.Add(
+                            new SqlParameter("@Quantity", line.Quantity)
+                            );
+
+                        commandDet.ExecuteNonQuery();
+
+                    }
+
+                    // Confirmando con la transacción con el commit
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+
+                    transaction.Rollback(); //Deshaciendo la transacción
+                }
+            }
+                       
+                return result;
+        }
     }
 }
---
+++ Cap02/slnApp/AppData.DataAccessTest/InvoiceDAtest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using AppEntities.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppData.DataAccessTest
+{
+    [TestClass]
+    public class InvoiceDAtest
+    {
+        [TestMethod]
+        public void Insert()
+        {
+            var invoice = new Invoice();
+            invoice.CustomerId = 1;
+            invoice.InvoiceDate = DateTime.Now;
+            invoice.BillingAddress = "";
+            invoice.BillingCity = "";
+            invoice.BillingState = "";
+            invoice.BillingCountry = "";
+            invoice.BillingPostalCode = "";
+            invoice.Total = 100;
+
+            invoice.InvoiceLine = new List<InvoiceLine>();
+
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 1,
+                    UnitPrice = 10,
+                    Quantity = 3
+                }
+            );
+
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 2,
+                    UnitPrice = 10,
+                    Quantity = 7
+                }
+            );
+        }
+
+    internal class List<T> : System.Collections.Generic.List<InvoiceLine>
+    {
+    }
+}
+}
