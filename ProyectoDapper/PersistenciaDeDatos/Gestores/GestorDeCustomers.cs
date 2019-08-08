e102cf6f3d971e3ea862e0b5f6ec460128da38a1
lanchorena7 <52942950+lanchorena7@users.noreply.github.com>
2019-08-07 22:25:36 -0500

    Dapper
    Dapper

---
+++ ProyectoDapper/PersistenciaDeDatos/Gestores/GestorDeCustomers.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using PersistenciaDeDatos.Compartido;
+using PersistenciaDeDatos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenciaDeDatos.Gestores
+{
+    public class GestorDeCustomers
+    {
+        private FabricaDeConexiones fabricaDeConexiones = new FabricaDeConexiones();
+
+        public List<Customer> ListarCustomers()
+        {
+            using (var conexion = fabricaDeConexiones.ConstruirConexionAChinook())
+            {
+                // EJECUTAR SQL EN EL CODIGO
+                //var listaDeCustomers = conexion.Query<Customer>(
+                //        "SELECT * FROM Customer",
+                //        commandType: CommandType.Text
+                //    ).ToList();
+
+                var listaDeCustomers = conexion.Query<Customer>(
+                        "ListarCustomers",
+                        commandType: CommandType.StoredProcedure
+                    ).ToList();
+                return listaDeCustomers;
+
+                //--SCRIPT DEL STORE PROCEDURE
+                //CREATE PROCEDURE ListarCustomers
+                //AS
+                //BEGIN
+                //    SELECT* FROM Customer
+                //END
+            }
+        }
+
+
+        public void InsertarGenre(string nombreDeGeneroMusical)
+        {
+            using (var conexion = fabricaDeConexiones.ConstruirConexionAChinook())
+            {
+                conexion.Execute(
+                        "INSERT INTO Genre(Name) VALUES(@Name)"
+                        , new { Name = nombreDeGeneroMusical }
+                        , commandType: CommandType.Text
+                    );
+            }
+        }
+
+    }
+}
---
+++ ProyectoDapper/Presentacion.Consola/Program.cs
@@ -0,0 +1,34 @@
+﻿using PersistenciaDeDatos.Gestores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gestorDeCustomers = new GestorDeCustomers();
+
+            gestorDeCustomers.ListarCustomers().ForEach(x =>
+            {
+                Console.WriteLine(x.Address);
+                
+            });
+
+            gestorDeCustomers
+                .ListarCustomers()
+                .Select(x => x.City)
+                .Distinct()
+                .ToList()
+                .ForEach(x => gestorDeCustomers.InsertarGenre(x));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
