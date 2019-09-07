b54b90005e49b17362fd73a31ece758978b48374
lanchorena7 <52942950+lanchorena7@users.noreply.github.com>
2019-09-06 22:36:44 -0500

    .
    -

---
+++ Clase06-09-19/Codigo/VisualStudio67-master/Shop.Presentation.WebPage/Scripts/LogicaDeNegocios/MantenimientoDeProductos.js
@@ -0,0 +1,82 @@
+﻿Vue.use(Toasted);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        categories: [],
+        categoryId: null,
+        categoryIdFilter:0,
+        nombre: '',
+        nombreFiltro:'',
+        descripcion: '',
+        listaDeProductos: [],
+        listaDeProductosSinFiltrar: [],
+        dialog: false
+    },
+    vuetify: new Vuetify(),
+    methods: {
+        GetCategoriesFromServer: function () {
+            $.get('/api/productmanagement/GetAllCategories')
+                .then(categoryArray => {
+                    this.categories.push(...categoryArray);
+                })
+                .catch(error => {
+                    let excepcion = JSON.parse(error.responseText);
+                    Vue.toasted.error(excepcion.MensajeDelError);
+                });
+        },
+        ResetearInterface: function () {
+            this.nombre = '';
+            this.descripcion = '';
+            this.categoryId = null;
+        },
+        RegistrarProductoEnElServidor: function () {
+                post('/api/productmanagement/RegisterProduct', {
+                    Name: this.nombre,
+                    Descripcion: this.descripcion,
+                    CategoryId: this.categoryId
+                })
+                .then(newProduct => {
+                    this.listaDeProductos.push(newProduct);
+                    this.ResetearInterface();
+                }).catch(error => {
+                    let excepcion = JSON.parse(error.responseText);
+                    Vue.toasted.error(excepcion.MensajeDelError);
+                });;
+        },
+        CargarProductosDelServidor: function () {
+            $.get('/api/productmanagement/GetAllProducts')
+                .then(productosDelServidor => {
+                    this.listaDeProductos.push(...productosDelServidor)
+                    this.listaDeProductosSinFiltrar.push(...productosDelServidor);
+                });
+        },
+        cambioElFiltro: function () {
+            if (this.categoryIdFilter == 0) {
+                this.listaDeProductos = [];
+                this.CargarProductosDelServidor();
+            }
+            else {
+                $.get('/api/productmanagement/GetProductByCategoryId?categoryId=' + this.categoryIdFilter)
+                    .then(productosDelServidor => {
+                        this.listaDeProductos = [];
+                        this.listaDeProductos.push(...productosDelServidor)
+                    });
+            }
+        },
+        ActivarFiltro: function () {
+            if (this.nombreFiltro.length > 0) {
+                this.listaDeProductos = [];
+                this.listaDeProductos = this.listaDeProductosSinFiltrar.filter(x => {
+                    return x.Name.includes(this.nombreFiltro);
+                });
+            }
+            else {
+                this.listaDeProductos = this.listaDeProductosSinFiltrar;
+            }
+
+        }
+    }
+})
+app.GetCategoriesFromServer();
+app.CargarProductosDelServidor();
---
+++ Clase06-09-19/Codigo/VisualStudio67-master/Shop.Presentation.WindowsService/Program.cs
@@ -0,0 +1,39 @@
+﻿using Quartz;
+using Shop.Presentation.WindowsService.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Quartz;
+
+namespace Shop.Presentation.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.UseNLog();
+                windowsService.ScheduleQuartzJobAsService(service => {
+                    service.WithJob(() => JobBuilder.Create<SincronizardorBaseDeDatos>().Build())
+                           .AddTrigger(() => TriggerBuilder
+                                            .Create()
+                                            .WithCronSchedule("0/5 * * * * ?")
+                                            .Build());
+                });
+                windowsService.StartAutomatically();
+                windowsService.EnableServiceRecovery(recoveryOptions =>
+                {
+                    recoveryOptions.RestartService(1);
+                });
+                windowsService.SetDisplayName("SincronizadorDeBaseDeDatos");
+                windowsService.SetDescription("Sincroniza las bases de datos");
+                windowsService.SetServiceName("SincronizadorSQLDeLaTienda");
+
+            });
+        }
+    }
+}
