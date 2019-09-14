7834d53c545fa9af323acd9c401528e3a4fb08bb
lanchorena7 <52942950+lanchorena7@users.noreply.github.com>
2019-09-13 22:21:30 -0500

    ventas
    ventas

---
+++ Clase13-09-19/Codigo/VentasVisualStudio201967-master/VentasVisualStudio201967-master/Ventas.Dominio.Entidades/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Dominio.Entidades.Compartido;
+using Ventas.Dominio.Entidades.Compartido.Enums;
+
+namespace Ventas.Dominio.Entidades
+{
+    public class Cliente: PersonaContacto
+    {
+        public DateTime FechaNacimiento { get; set; }
+        public Sexo Sexo { get; set; }
+        public HashSet<Venta> Ventas { get; set; }
+    }
+}
---
+++ Clase13-09-19/Codigo/VentasVisualStudio201967-master/VentasVisualStudio201967-master/Ventas.Dominio.Entidades/DetalleVenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Dominio.Entidades.Compartido;
+
+namespace Ventas.Dominio.Entidades
+{
+    public class DetalleVenta: EntidadBase
+    {
+        public int Cantidad { get; set; }
+        public double SubTotal { get; set; }
+        public int ProductoId { get; set; }
+        public int VentaId { get; set; }
+        public Venta Venta { get; set; }
+        public Producto Producto { get; set; }
+    }
+}
