31a0de42a161ed663487141db6adb19ef49acd7f
lanchorena7 <52942950+lanchorena7@users.noreply.github.com>
2019-09-16 22:26:42 -0500

    .
    .

---
+++ Clase16-09-19/Codigo/VentasVisualStudio201967-master/VentasVisualStudio201967-master/Ventas.Dominio.Entidades/Compartido/Enums/TipoDeVenta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.Dominio.Entidades.Compartido.Enums
+{
+    public enum TipoDeVenta
+    {
+        Boleta = 1,
+        Factura = 2
+    }
+}
---
+++ Clase16-09-19/Codigo/VentasVisualStudio201967-master/VentasVisualStudio201967-master/Ventas.Servicios.ImplementacionConSQL/GestorDeClientes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Infraestructura.ContextoDeDatos;
+using Ventas.Servicios.Interfacez;
+using Ventas.Servicios.Interfacez.Peticiones;
+using Ventas.Servicios.Interfacez.Respuestas;
+using Ventas.Servicios.MapaDeConversiones;
+
+namespace Ventas.Servicios.ImplementacionConSQL
+{
+    public class GestorDeClientes : IGestorDeClientes
+    {
+        public ClienteCompleto ActualizarCliente(ActualizacionDeCliente actualizacionDeCliente)
+        {
+            using (var db = new VentasDB())
+            {
+                var cliente = db.Clientes.Find(actualizacionDeCliente.Id);
+                actualizacionDeCliente.ActualizarRegistro(cliente);
+                db.SaveChanges();
+                return cliente.ConvertirADTOCompleto();
+            }
+        }
+
+        public ClienteCompleto BuscarPorId(int Id)
+        {
+            using (var db = new VentasDB())
+            {
+                return db.Clientes.Find(Id).ConvertirADTOCompleto();
+            }
+        }
+
+        public IEnumerable<ClienteRegistrado> ListarTodosLosClientes()
+        {
+            using (var db = new VentasDB())
+            {
+                return db.Clientes.ToList().Select(x=>x.ConvertirADTO());
+            }
+        }
+
+        public ClienteCompleto RegistrarNuevoCliente(NuevoCliente nuevoCliente)
+        {
+            using (var db = new VentasDB())
+            {
+                var cliente = nuevoCliente.ConvertirAEntidad();
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                return cliente.ConvertirADTOCompleto();
+            }
+        }
+    }
+}
